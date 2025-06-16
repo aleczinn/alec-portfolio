d40a107f98a28a5d30b4413e3bfcd3ba00a1a314
Alec <alec_z17@web.de>
2025-06-16 15:20:12 +0200

    Updates

---
+++ assets/chevron-down-BRoBJ8ye.js
@@ -0,0 +1,26 @@
+import{m as c}from"./index-BFrpASdE.js";/**
+ * @license lucide-vue-next v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase(),h=e=>e.replace(/^([A-Z])|[\s-_]+(\w)/g,(t,r,o)=>o?o.toUpperCase():r.toLowerCase()),w=e=>{const t=h(e);return t.charAt(0).toUpperCase()+t.slice(1)},C=(...e)=>e.filter((t,r,o)=>!!t&&t.trim()!==""&&o.indexOf(t)===r).join(" ").trim();/**
+ * @license lucide-vue-next v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var s={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"};/**
+ * @license lucide-vue-next v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=({size:e,strokeWidth:t=2,absoluteStrokeWidth:r,color:o,iconNode:l,name:n,class:g,...u},{slots:a})=>c("svg",{...s,width:e||s.width,height:e||s.height,stroke:o||s.stroke,"stroke-width":r?Number(t)*24/Number(e):t,class:C("lucide",...n?[`lucide-${i(w(n))}-icon`,`lucide-${i(n)}`]:["lucide-icon"]),...u},[...l.map(d=>c(...d)),...a.default?[a.default()]:[]]);/**
+ * @license lucide-vue-next v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const p=(e,t)=>(r,{slots:o})=>c(m,{...r,iconNode:t,name:e},o);/**
+ * @license lucide-vue-next v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=p("chevron-down",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]);export{k as C,p as c};
