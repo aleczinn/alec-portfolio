8317c77f3736c31b0e0ce54325d0c47990723dbd
Alec <alec_z17@web.de>
2025-06-17 19:33:55 +0200

    Updates

--- assets/chevron-down-b2hhlVLZ.js
+++ assets/chevron-down-b2hhlVLZ.js
@@ -1,4 +1,4 @@
-import{c as o}from"./createLucideIcon-DRYmAbC1.js";/**
+import{c as o}from"./createLucideIcon-BGizBSdn.js";/**
  * @license lucide-vue-next v0.511.0 - ISC
  *
  * This source code is licensed under the ISC license.
