d26a0971d70b82d7e62b12c497b768d9ed782a67
Alec <alec_z17@web.de>
2025-06-17 21:04:35 +0200

    Updates

--- assets/linkedin-CBsE4XfR.js
+++ assets/linkedin-CBsE4XfR.js
@@ -1,4 +1,4 @@
-import{c as e}from"./createLucideIcon-DjzLQzaC.js";/**
+import{c as e}from"./chevron-down-DTJ1zJoS.js";/**
  * @license lucide-vue-next v0.511.0 - ISC
  *
  * This source code is licensed under the ISC license.
