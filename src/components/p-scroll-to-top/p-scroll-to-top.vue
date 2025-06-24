76dbb4850769f54f03bbff7c414b932693faf599
Alec <alec_z17@web.de>
2025-06-24 13:59:51 +0200

    changed positioning for a better look

--- src/components/p-scroll-to-top/p-scroll-to-top.vue
+++ src/components/p-scroll-to-top/p-scroll-to-top.vue
@@ -1,6 +1,6 @@
 <template>
     <a @click="scrollToTop"
-       class="fixed bottom-24 right-8 w-14 h-14 rounded-full bg-new-light/60 backdrop-blur-md flex justify-center items-center transition-all duration-300 hover:bg-new-lightest hover:cursor-pointer z-50"
+       class="fixed bottom-10 right-8 w-14 h-14 rounded-full bg-new-light/60 backdrop-blur-md flex justify-center items-center transition-all duration-300 hover:bg-new-lightest hover:cursor-pointer z-50"
        :class="{ 'opacity-0 pointer-events-none': !showScrollTop, 'opacity-100': showScrollTop }"
     >
         <chevron-up-icon class="w-6 h-6 text-white mb-[0.125rem]"/>
