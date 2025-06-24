32f3b60020f70ea2611f99495ba23d19d7c35f06
Alec <alec_z17@web.de>
2025-06-24 10:39:08 +0200

    reworked header (missing mobile func)

--- src/components/p-header/p-header.vue
+++ src/components/p-header/p-header.vue
@@ -1,33 +1,32 @@
 <template>
-    <header class="fixed top-4 md:top-8 left-1/2 transform -translate-x-1/2 z-50 w-[calc(100%-2rem)] lg:w-auto max-w-[calc(100vw-2rem)]">
-        <nav class="backdrop-blur-md bg-black/40 border-2 border-new/20 rounded-2xl md:rounded-3xl px-4 md:px-8 py-4 md:py-6 shadow-2xl">
-            <ul class="flex items-center justify-center gap-2 md:gap-6">
-                <li v-for="(item, index) in navItems" :key="index" class="flex-shrink-0">
-                    <a :href="`#${item.id}`"
-                       :class="[
-                           'px-2 md:px-4 py-1 md:py-2 transition-all duration-300 text-sm md:text-base whitespace-nowrap block',
-                           activeSection === item.id
-                               ? 'text-new-light font-bold'
-                               : 'text-white/80 hover:text-white font-normal'
-                       ]">
-                        {{ item.name }}
-                    </a>
-                </li>
+    <header class="header">
+        <div class="header-container">
+            <div class="block lg:hidden hover:cursor-pointer" @click="toggleMobileMenu">
+                <MenuIcon></MenuIcon>
+            </div>
 
-                <li class="flex-shrink-0 ml-2 md:ml-0">
-                    <a @click="handleToggleLanguage"
-                       class="p-1 md:p-0 transition-all duration-300 text-white/80 hover:text-white hover:cursor-pointer block">
-                        <component :is="GlobeIcon" class="w-5 h-5 md:w-[1.375rem] md:h-[1.375rem]"/>
-                    </a>
-                </li>
-            </ul>
-        </nav>
+            <nav class="hidden lg:block">
+                <ul class="flex flex-row justify-center items-center gap-12">
+                    <li v-for="(item, index) in navItems" :key="index" class="">
+                        <a :href="`#${item.id}`">
+                            {{ item.name }}
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div class="ml-12" @click="toggleLanguage">
+                <GlobeIcon></GlobeIcon>
+            </div>
+        </div>
     </header>
 </template>
 
 <script setup lang="ts">
-import { Globe as GlobeIcon } from "lucide-vue-next";
-import { useI18n } from "vue-i18n";
+import { ref } from 'vue'
+import { Globe as GlobeIcon, Menu as MenuIcon, X as XIcon } from "lucide-vue-next"
+import { useI18n } from "vue-i18n"
+
 const { t } = useI18n()
 
 interface Props {
@@ -40,13 +39,32 @@ const props = withDefaults(defineProps<Props>(), {
     activeSection: ''
 })
 
+const isMenuOpen = ref(false)
+
 const emit = defineEmits(['toggle-language'])
 
-const handleToggleLanguage = () => {
+const toggleLanguage = () => {
     emit('toggle-language')
+}
+
+const toggleMobileMenu = () => {
+    isMenuOpen.value = !isMenuOpen.value
+    console.log("toggleMobileMenu", isMenuOpen.value);
+}
+
+const closeMobileMenu = () => {
+    isMenuOpen.value = false
 }
 </script>
 
 <style scoped lang="postcss">
+.header {
+    @apply sticky top-0 bg-black/40 px-6 py-6 z-50;
+    @apply lg:absolute lg:top-8 lg:left-1/2 lg:transform lg:-translate-x-1/2 lg:rounded-3xl lg:w-auto lg:min-w-max;
+    @apply shadow-lg;
+}
 
+.header .header-container {
+    @apply flex flex-row justify-between;
+}
 </style>
