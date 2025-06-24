11728dc777f03043c0e59ac0c8460954431509f4
Alec <alec_z17@web.de>
2025-06-24 14:14:42 +0200

    changed section to new p-section, added simple image

--- src/components/p-section-about-me/p-section-about-me.vue
+++ src/components/p-section-about-me/p-section-about-me.vue
@@ -1,36 +1,35 @@
 <template>
-    <section class="py-32">
-        <div class="mx-auto max-w-portfolio px-6">
-            <h2 class="text-3xl md:text-4xl font-bold mb-16 text-center">{{ t('about-me.title') }}</h2>
+    <p-section>
+        <h2 class="text-3xl md:text-4xl font-bold mb-16 text-center">{{ t('about-me.title') }}</h2>
 
-            <div class="grid grid-cols-1 lg:grid-cols-2">
-                <div class="p-6 w-full">
-                    <div class="aspect-w-16 aspect-h-9">
-                        <div class="w-full h-full rounded-2xl overflow-hidden">
-                            <!--                                    <img src="/image-1.png" alt="Alec" class="w-full h-full object-cover" />-->
-                        </div>
+        <div class="grid grid-cols-1 lg:grid-cols-2">
+            <div class="p-6 w-full">
+                <div class="aspect-w-16 aspect-h-9">
+                    <div class="w-full h-full rounded-2xl overflow-hidden bg-white/5">
+                        <img src="/image-1.png" alt="Alec" class="w-full h-full object-cover" />
                     </div>
                 </div>
+            </div>
 
-                <div class="p-6 w-full">
-                    <p class="text-lg mb-8">{{ t('about-me.text1')}}</p>
-                    <p class="text-lg mb-12">{{ t('about-me.text2')}}</p>
-                    <div class="grid grid-cols-2 md:grid-cols-4 gap-6">
-                        <div v-for="(stat, index) in stats" :key="index"
-                             class=" p-4 text-center">
-                            <div class="text-3xl text-new-light font-bold mb-1">{{ stat.value }}</div>
-                            <div class="text-sm text-white">{{ stat.label }}</div>
-                        </div>
+            <div class="p-6 w-full flex flex-col justify-items-center">
+                <p class="text-lg mb-8">{{ t('about-me.text1')}}</p>
+                <p class="text-lg mb-12">{{ t('about-me.text2')}}</p>
+                <div class="grid grid-cols-2 md:grid-cols-4 gap-6">
+                    <div v-for="(stat, index) in stats" :key="index"
+                         class=" p-4 text-center">
+                        <div class="text-3xl text-new-light font-bold mb-1">{{ stat.value }}</div>
+                        <div class="text-sm text-white">{{ stat.label }}</div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </p-section>
 </template>
 
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
 import { computed } from "vue";
+import { PSection } from "../p-section";
 
 const { t } = useI18n()
 
